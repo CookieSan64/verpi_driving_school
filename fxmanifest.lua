7612e5c23e67a201352cb60d764814656ef0accd
ChouCookieSan <choucookiesan@gmail.com>
2024-03-08 02:33:58 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+author "ChouCookieSan"
+description "Auto-école"
+version "1.0.0"
+
+fx_version "cerulean"
+game "gta5"
+
+this_is_a_map 'yes'
